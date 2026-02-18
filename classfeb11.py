347d2539b478a3d03990a200fb22abc4cc563ab5
Noor Ul Huda <157737367+noorulhuda455@users.noreply.github.com>
2026-02-18 10:23:10 -0500

    adding mutiplication + division

--- classfeb11.py
+++ classfeb11.py
@@ -1,20 +1,28 @@
 import ply.lex as lex
+import ply.yacc as yacc
 
 # -------------------
 # TOKENS
 # -------------------
 
+#1. Define the list of tokens 
+
 tokens = (
     'NUMBER',
     'PLUS',
     'MINUS',
+    'TIMES',
+    'DIVIDE',
 )
 
-t_PLUS  = r'\+'
-t_MINUS = r'-'
+t_PLUS   = r'\+'
+t_MINUS  = r'-'
+t_TIMES  = r'\*'
+t_DIVIDE = r'/'
 
 def t_NUMBER(t):
     r'[0-9]+'
+    t.value = int(t.value)
     return t
 
 t_ignore = ' \t'
@@ -32,14 +40,57 @@ text_input = "5+4-2"
 
 # Print formatted expression
 formatted = text_input.replace("+", " + ").replace("-", " - ")
-print("evaluating: ", formatted)
 
-# Send input to lexer
-lexer.input(text_input)
+#commenting the code below 
 
-# Print token types
-while True:
-    tok = lexer.token()
-    if not tok:
+# print("evaluating: ", formatted)
+
+# # Send input to lexer
+# lexer.input(text_input)
+
+# # Print token types
+# while True:
+#     tok = lexer.token()
+#     if not tok:
+#         break
+#     print(tok.type)
+
+
+#code from his presentation
+
+#yacc rules
+def p_expression_number(p):
+    'expression : NUMBER'
+    p[0] = p[1]
+
+def p_expression_binop(p):
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression TIMES expression
+                  | expression DIVIDE expression'''
+
+    if p[2] == '+':
+        p[0] = p[1] + p[3]
+    elif p[2] == '-':
+        p[0] = p[1] - p[3]
+    elif p[2] == '*':
+        p[0] = p[1] * p[3]
+    elif p[2] == '/':
+        p[0] = p[1] / p[3]
+
+
+#3. Build the parser 
+
+parser = yacc.yacc()
+
+#4. Test it out 
+
+while True: 
+    try: 
+        s = input('calc > ')
+    except EOFError:
         break
-    print(tok.type)
+    if not s: 
+        continue
+    result = parser.parse(s)
+    print(f"Result: {result}")
